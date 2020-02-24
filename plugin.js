536ba0ce6b7dea11d5c680e4d961e086641ee1c9
ac2pic <ac2pic@gmail.com>
2020-02-23 19:25:34 -0600

    Added basic stuff

---
+++ plugin.js
@@ -0,0 +1,18 @@
+export default class MapLoad extends Plugin {
+	constructor(mod) {
+		super();
+		this.mod = mod;
+	}
+
+	async preload() {
+
+	}
+
+	async postload() {
+
+	}
+
+	async prestart() {
+
+	}
+}
