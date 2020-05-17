b21ffce6b254e71aec887759ab558ceb13f9ed72
ac2pic <ac2pic@gmail.com>
2020-05-17 11:18:33 -0500

    Updated to reflect changes made to Tool-Launcher

--- map-loader-client.js
+++ map-loader-client.js
@@ -1,6 +1,18 @@
-const {Client} = window.ToolsApi.Communication;
+const { Client } = window.Tool.Communication;
 
 export default class ReloaderClient extends Client {
+    constructor() {
+        super();
+        this.init();
+    }
+
+    init() {
+        this.subscribeTo("map");
+        window.addEventListener("beforeunload", () => {
+            this.unsubscribeFrom("map");
+        });
+    }
+
     onMessage(topic, msg) {
         if (topic === 'map' && msg.type === 'update') {
             window.LOAD_RAW_MAP = msg.data;
--- plugin.js
+++ plugin.js
@@ -1,5 +1,5 @@
 import ReloaderClient from "./map-loader-client.js";
-const {Api} = window.ToolsApi.Communication;
+const { Topic } = window.Tool.Communication;
 
 export default class MapLoad extends Plugin {
 	constructor(mod) {
@@ -8,16 +8,9 @@ export default class MapLoad extends Plugin {
 		this.client = new ReloaderClient;
 	}
 
-	preload() {
-		Api.subToTopic("map", this.client);
-		window.addEventListener("beforeunload", () => {
-			Api.unsubFromTopic("map", this.client);
-		});
-	}
-
 	async prestart() {
 		sc.CrossCode.inject({
-			init: function(...args) {
+			init: function (...args) {
 				this.parent(...args);
 				sc.mapLoader.setTitleGui(ig.gui.guiHooks.filter(e => e.gui instanceof sc.TitleScreenGui).pop().gui);
 			}
